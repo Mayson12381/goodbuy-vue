6d68f04be4ea3ebd96b92481adc251b2de908427
Darjusch <32839416+Darjusch@users.noreply.github.com>
2020-03-16 14:37:05 +0100

    Update FeatureView.vue
    Delted Slots

--- src/components/feature/FeatureView.vue
+++ src/components/feature/FeatureView.vue
@@ -1,10 +1,7 @@
 <template>
     <div class="feature-view">
         <FeatureViewHeaderBar />
-        <FeatureViewContent>
-            <slot slot="karma-fridge"></slot> 
-            <slot slot="instant-feedback"></slot> 
-        </FeatureViewContent>
+        <FeatureViewContent />
     </div>
 </template>
 
