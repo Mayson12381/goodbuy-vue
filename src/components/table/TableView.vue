20a85897a0f1f466535b498b61cc598571ddb5f0
Anthony <anthony.sherrill@code.berlin>
2020-03-19 12:28:52 +0100

    Working TableView
    We altered the html structure and naming of the elements. Replaced the whole styling.

--- src/components/table/TableView.vue
+++ src/components/table/TableView.vue
@@ -1,57 +1,61 @@
 <template>
-    <div>
-        <HeaderBar></HeaderBar>
-        <div class="container">
-            <template v-for="(data, index) in response_data" style="overflow-y: auto;">
-                <button
-                    @click="isProductInfoActive = true, 
-                    product = data.fields"
-                    :key="index"
-                    class="router-style"
-                >
-                    {{ data.fields.name }}
-                    <div class="is-big-ten">
-                        <div v-if="data.is_big_ten == true">
-                            ❌
-                        </div>
-                        <div v-if="data.is_big_ten == false">
-                            ✅
-                        </div>
-                        <div v-if="data.is_big_ten == 'We don\'t know'">
-                            ❓
-                        </div>
-                    </div>
-                </button>
-                <hr :key="index+data.fields.name">
-            </template>
-
-            <transition name="slide-up">
-                <InfoSlideUp
-                    v-if="isProductInfoActive"
-                    :showScanAgainButton="false"
-                    :productName="product.name"
-                    :productBrand="product.brand"
-                    :productCorporation="product.corporation"
-                    :barcode="this.barcode"
-                    @closeInfoModal="isProductInfoActive = false"
-                />
-            </transition>
-        </div>
-            <div class="footer-score">
-                <p class="p-score">Score: {{ score }}</p>
-                <button 
-                class="done-button"
-                @click="reRouteToFeature"
-                >
-                Done
-                </button>
-            </div>
+  <div class="container">
+    <div class="header">
+      <HeaderBar></HeaderBar>
     </div>
+    <div class="main">
+      <template v-for="(data, index) in response_data">
+          <button
+              @click="isProductInfoActive = true,
+              product = data.fields"
+              :key="index"
+              class="row"
+              >
+              {{ data.fields.name }}
+              <div class="is-big-ten">
+                  <div v-if="data.is_big_ten == true">
+                      ❌
+                  </div>
+                  <div v-if="data.is_big_ten == false">
+                      ✅
+                  </div>
+                  <div v-if="data.is_big_ten == 'We don\'t know'">
+                      ❓
+                  </div>
+              </div>
+          </button>
+          <hr :key="index+data.fields.name">
+      </template>
+    </div>
+
+    <transition name="slide-up">
+        <InfoSlideUp
+            v-if="isProductInfoActive"
+            :showScanAgainButton="false"
+            :productName="product.name"
+            :productBrand="product.brand"
+            :productCorporation="product.corporation"
+            :barcode="this.barcode"
+            @closeInfoModal="isProductInfoActive = false"
+        />
+    </transition>
+
+    <div class="footer">
+        <p class="p-score">Score: {{ score }}</p>
+        <button
+        class="done-button"
+        @click="reRouteToFeature"
+        >
+        Done
+        </button>
+    </div>
+  </div>
 </template>
+
 <script>
 import FeedbackService from '@/FeedbackService'
 import InfoSlideUp from '@/components/product/info/InfoSlideUp.vue'
-import HeaderBar from '@/components/intro/IntroViewHeaderBar.vue'
+import HeaderBar from '@/components/feature/FeatureViewHeaderBar.vue'
 
 export default {
     components: {
@@ -74,7 +78,6 @@ export default {
                 code: '',
             },
             barcode: '',
-
         }
     },
     methods: {
@@ -96,13 +99,13 @@ export default {
             this.response_data.forEach(element => {
                 this.big_size += 1
                 if (element.is_big_ten == true) {
-                    this.big_true += 1 
+                    this.big_true += 1
                 }
                 else if (element.is_big_ten == false) {
-                    this.big_false += 1 
+                    this.big_false += 1
                 }
                 else {
-                    this.big_we_dont_know += 1 
+                    this.big_we_dont_know += 1
                 }
             })
             // <!-- Good Item / Total Item * 100 = Score -->
@@ -121,47 +124,51 @@ export default {
     }
 }
 </script>
+
 <style scoped>
-    .router-style {
-        text-decoration: none;
-    }
-    .router-style:visited {
-        color: black;
-    }
-    .is-big-ten {
-       margin-left: 80%;
-    }
-    .footer-score {
-        padding: .3rem;
-        overflow: hidden;
-        background-color: #272727;
-        display: flex;
-        align-items: center;
-        justify-content: flex-start;
-        position: fixed;
-        bottom: 0;
-        width: 100%
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Avenir';
+}
 
-    }
-    .p-score {
-        margin: auto;
-        text-align: center;
-        font-family: 'Avenir';
-        color: white;
-        font-size: 12vw;
-    }
-    .done-button {
-        margin: auto;
-        text-align: center;
-        font-family: 'Avenir';
-        color: white;
-        font-size: 12vw;
-    }
+.container {
+  display: flex;
+  flex-direction: column;
+  width:  100%;
+  height: 100%;
+}
 
-    #app {
- 
-        overflow: scroll;
+.main {
+  table-layout:fixed;
+  flex: 5;
+  overflow-y: auto;
+}
 
-    }
+.row {
+  display: flex;
+  justify-content: space-around;
+  font-size: 8vw;
+  width: 100%;
+}
 
+.footer {
+  background-color: #272727;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex: 1;
+  color: white;
+  font-size: 12vw;
+}
+
+button.done-button {
+  color: white;
+  font-size: 12vw;
+  background-color: #272727;
+  border-radius: 16px;
+  margin-right: 20px;
+  padding: 1vw;
+}
 </style>
