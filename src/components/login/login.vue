df8b827a7a5158fc38c1f1f1087641a1f7a58203
Anthony <anthony.sherrill@code.berlin>
2020-04-19 13:23:57 +0200

    cleaned up a bit

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -21,41 +21,6 @@ const API_URL = 'http://localhost:8080'
 
 export default {
   name: 'login',
-  // data () {
-  //   return {
-  //     authenticated: false,
-  //     message: ''
-  //   }
-  // },
-  // mounted() {
-  //   this.handleAuthentication()
-  //   this.authenticated = false
-  //
-  //   auth.authNotifier.on('authChange', authState => {
-  //     this.authenticated = authState.authenticated
-  //   })
-  // },
-  // methods: {
-  //   login() {
-  //     auth.login()
-  //   },
-  //   handleAuthentication() {
-  //     auth.handleAuthentication()
-  //   },
-  //   logout () {
-  //     auth.logout()
-  //     localStorage.setItem('user_id', '')
-  //     localStorage.setItem('auth_token', '')
-  //   },
-  //   privateMessage() {
-  //     localStorage.setItem('user_id', auth.profile.sub.split('|')[1])
-  //     localStorage.setItem('auth_token', auth.getAuthToken())
-  //     const url = `${API_URL}/product/barcode/12/`
-  //     return axios.get(url, {headers: {Authorization: `Bearer ${auth.getAuthToken()}`}}).then((response) => {
-  //       this.message = response.data || ''
-  //     })
-  //   }
-  // }
   methods: {
     // Log the user in
     login() {
